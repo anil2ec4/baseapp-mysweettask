764c74403449e83f5783f95fef719ea04a74d3e0
anil2ec4 <anil2ec4@users.noreply.github.com>
2025-10-16 21:49:30 +0300

    update

--- minikit.config.ts
+++ minikit.config.ts
@@ -14,7 +14,9 @@ export const minikitConfig = {
     "signature": "LXKpsm6qhOA6Bb5vv5iU0yAUY7YgefaeBC8HoFNVj1cXQayEYvLm5ItzbPvPkMBhzBVsIolFLFDmCCsUlUibTRw="
   },
   "baseBuilder": {
-    "ownerAddress": "0xe8cc79ceaabf3ddf939469452341579ece2a5611"
+    "allowedAddresses": [
+      "0xe8cc79ceaabf3ddf939469452341579ece2a5611"
+    ]
   },
   miniapp: {
     version: "1",
